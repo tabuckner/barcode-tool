ad688a8d1b49fd52c4d9d473c12763a569fd9312
Taylor Buckner <tbuckner@theappraisallane.com>
2019-03-07 18:13:54 -0600

    Added vin-generator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import _QRCode from 'qrcode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'barcode-tool';
+  public randomVin: string;
+  public base64Image: string;
+  // private vinGenerator;
+  private QRCode;
 
-  constructor(private http: HttpClient) {
-    this.http.get('https://randomvin.com/getvin.php?type=real').subscribe(data => console.log(data));
+  constructor() {
+    // this.vinGenerator = _VinGenerator;
+    this.QRCode = _QRCode;
+  }
+
+  public ngOnInit() {
+    this.getQrCode();
+  }
+
+  public getQrCode() {
+    this.setRandomVin();
+    this.getQrCodeDataUrl(this.randomVin)
+      .then(url => {
+        this.base64Image = url;
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  public setRandomVin() {
+    const dirtyVin = 'WDBEB28D6MB240164'; // TODO: Create a lazy library to serve a few options.
+    let cleanVin = dirtyVin.substring(0, dirtyVin.length - 6);
+    for (let i = 0; i < 6; i++) {
+      cleanVin += Math.floor(Math.random() * 10);
+    }
+    console.log(cleanVin, cleanVin.length);
+    this.randomVin = cleanVin;
+  }
+
+  public getQrCodeDataUrl(data: string): Promise<string> {
+    return this.QRCode.toDataURL(data);
   }
 }
